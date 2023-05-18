3a29ae6d5b87fc7fa68621813d1977f60db4fc26
gzy101 <61415801+gzy101@users.noreply.github.com>
2023-05-18 22:20:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import random
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+birthday1 = os.environ['BIRTHDAY1']
+birthday2 = os.environ['BIRTHDAY2']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
@@ -24,8 +25,14 @@ def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+def get_birthday1():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday1, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_birthday2():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday2, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
@@ -46,7 +53,8 @@ wm = WeChatMessage(client)
 data = {
         "gzy_test":{"value":"27", "color":get_random_color()},
         "love_days":{"value":get_count(), "color":get_random_color()},
-        "birthday_left":{"value":get_birthday(), "color":get_random_color()},
+        "birthday1_left":{"value":get_birthday1(), "color":get_random_color()},
+        "birthday2_left":{"value":get_birthday2(), "color":get_random_color()},
         "words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
